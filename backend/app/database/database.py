bc371ea05e6082f8a5fd886ee9d3a51025804f10
Benjamin Inglis <bjammin134@gmail.com>
2026-01-23 14:16:22 -0800

    Create relationship between users and sessions

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -7,6 +7,9 @@ db_url = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@lo
 engine = create_engine(db_url, echo=True)
 
 def init_db():
+    from ..models.conversation_session import ConversationSession
+    from ..models.user import User
+    
     SQLModel.metadata.create_all(engine)
 
 def get_db():
--- backend/app/database/seed.py
+++ backend/app/database/seed.py
@@ -2,8 +2,8 @@
 Seed the database with initial data for development.
 """
 from sqlmodel import Session, select
-from ..database.database import get_db
 from ..models.user import User
+from ..models.conversation_session import ConversationSession
 from ..utils.password import hash_password
 
 def seed_users(db: Session):
@@ -40,15 +40,44 @@ def seed_users(db: Session):
             target_language="spanish",
         ),
     ]
-    
     db.add_all(test_users)
     db.commit()
     
     print(f"Seeded {len(test_users)} users.")
 
+def seed_conversations(db: Session):
+    test_statement = select(ConversationSession)
+    results = db.exec(test_statement)
+    conversation_sessions = results.all()
+    conversation_session_count = len(conversation_sessions)
+    
+    if conversation_session_count > 0:
+        print(f"Database already has {conversation_session_count} users. Skipping seed.")
+        return
+    statement = select(User).where(User.role == "student")
+    student = db.exec(statement).first()
+    
+    test_conversations = [
+        ConversationSession(
+            conversation_prompt="test",
+            cultural_context="mexico",
+            target_language="cat",
+            topic="meow",
+            tenses="present, preterite",
+            vocabulary="meow, meow",
+            difficulty_level="hard",
+            student_id = student.id
+        ),
+    ]
+
+    db.add_all(test_conversations)
+    db.commit()
+
+    print(f"Seeded {len(test_conversations)} conversations.")
 
 def seed_all(db: Session):
     """Run all seed functions."""
     seed_users(db)
+    seed_conversations(db)
     # seed_vocabulary(db)  # Add more seeders as needed
     # seed_topics(db)
