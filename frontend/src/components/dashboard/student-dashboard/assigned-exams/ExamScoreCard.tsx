8411ff7af8bae762dddab54be272d9b4912656cd
Benjamin Inglis <bjammin134@gmail.com>
2026-02-03 09:44:30 -0800

    Create ExamScoreCard visual layout

--- frontend/src/components/dashboard/student-dashboard/assigned-exams/ExamScoreCard.tsx
+++ frontend/src/components/dashboard/student-dashboard/assigned-exams/ExamScoreCard.tsx
@@ -1,9 +1,37 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Separator, Text, VStack } from "@chakra-ui/react";
+import { type SessionScoreResponse } from "@/utils/apiCalls";
 
-export default function ExamScoreCard() {
+interface ExamScoreCardProps {
+  examScoreData: SessionScoreResponse;
+}
+
+export default function ExamScoreCard({ examScoreData }: ExamScoreCardProps) {
   return (
-    <Flex>
-      <Text>Exam scorecard</Text>
-    </Flex>
+    <VStack>
+      <Text fontSize="2xl" fontWeight="bold">
+        Exam Score
+      </Text>
+      <Flex gap={4}>
+        <VStack>
+          <Text fontWeight="bold">Vocabulary Usage Score</Text>
+          <Text>{examScoreData.vocabulary_usage_score * 100}</Text>
+        </VStack>
+        <Separator orientation="vertical" variant="solid" size="md" />
+        <VStack>
+          <Text fontWeight="bold">Fluency Score</Text>
+          <Text>{examScoreData.fluency_score * 100}</Text>
+        </VStack>
+        <Separator orientation="vertical" variant="solid" size="md" />
+        <VStack>
+          <Text fontWeight="bold">Verb Tense Accuracy Score</Text>
+          <Text> {examScoreData.verb_tense_accuracy_score * 100}</Text>
+        </VStack>
+        <Separator orientation="vertical" variant="solid" size="md" />
+        <VStack>
+          <Text fontWeight="bold">Overall Score</Text>
+          <Text>{examScoreData.overall_score * 100}</Text>
+        </VStack>
+      </Flex>
+    </VStack>
   );
 }
