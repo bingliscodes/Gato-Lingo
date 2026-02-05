1028cc87f758e96979f49e72bbd8d6c45bcadbc4
Benjamin Inglis <bjammin134@gmail.com>
2026-02-05 13:06:45 -0800

    Display vocabulary lists as a carousel

--- frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/CreatedVocabularyLists.tsx
+++ frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/CreatedVocabularyLists.tsx
@@ -1,17 +1,57 @@
+import { Flex, Text, Carousel, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+
 import { useVocabularyLists } from "@/hooks/useVocabularyLists";
-import { Flex, Text } from "@chakra-ui/react";
+import VocabularyListCard from "./VocabularyListCard";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
 export default function CreatedVocabularyLists() {
   const { vocabLists, isLoading, error } = useVocabularyLists();
-
+  const [page, setPage] = useState(0);
   if (isLoading) return <div>Loading vocabulary lists... </div>;
   if (error) return <div>An error has occurred: {error} </div>;
   console.log(vocabLists);
   return (
-    <Flex gap={2} mx={1} flex="1" justify="center">
-      <Text fontWeight="bolder" fontSize="3xl">
-        Vocabulary Lists
-      </Text>
+    <Flex gap={2} mx={1} justify="center">
+      <Flex flexDir="column">
+        <Text fontWeight="bolder" fontSize="3xl">
+          Vocabulary Lists
+        </Text>
+
+        <Carousel.Root
+          slideCount={vocabLists.length}
+          maxW="md"
+          mx="auto"
+          page={page}
+          onPageChange={(e) => setPage(e.page)}
+        >
+          <Carousel.ItemGroup>
+            {vocabLists.map((list, idx) => (
+              <Carousel.Item key={list.id} index={idx}>
+                <VocabularyListCard vocabularyListData={list} />
+              </Carousel.Item>
+            ))}
+          </Carousel.ItemGroup>
+
+          <Carousel.Control justifyContent="center" gap="4">
+            <Carousel.PrevTrigger asChild>
+              <IconButton size="xs" variant="ghost">
+                <LuChevronLeft />
+              </IconButton>
+            </Carousel.PrevTrigger>
+            <Carousel.Indicators />
+
+            <Carousel.NextTrigger asChild>
+              <IconButton size="xs" variant="ghost">
+                <LuChevronRight />
+              </IconButton>
+            </Carousel.NextTrigger>
+          </Carousel.Control>
+        </Carousel.Root>
+        {/* {vocabLists.map((list) => (
+          <VocabularyListCard key={list.id} vocabularyListData={list} />
+        ))} */}
+      </Flex>
     </Flex>
   );
 }
--- frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/VocabularyListCard.tsx
+++ frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/VocabularyListCard.tsx
@@ -14,33 +14,11 @@ export default function VocabularyListCard({
       <Card.Header fontSize="lg" fontWeight="bold">
         {vocabularyListData.title}
       </Card.Header>
-      <Card.Body gap={1}>
-        <Text>Description: {vocabularyListData.description}</Text>
+      <Card.Body>
+        <Text fontWeight="bold">Description:</Text>
+        <Text>{vocabularyListData.description}</Text>
       </Card.Body>
+      <Card.Footer></Card.Footer>
     </Card.Root>
   );
 }
-
-<Card.Root w="60%">
-  <Card.Header fontSize="lg" fontWeight="bold">
-    {examData.exam.title}
-  </Card.Header>
-  <Card.Body gap={1}>
-    <Text>Topic: {examData.exam.topic}</Text>
-    <Text>Description: {examData.exam.description}</Text>
-    <Text>Tenses: {examData.exam.tenses}</Text>
-    <Text>Vocabulary: {examData.exam.vocabulary_list_manual}</Text>
-    <Text>
-      Status: {examData.completed}/{examData.total_assigned} completed,{" "}
-      {examData.in_progress} in progress, {examData.pending} pending
-    </Text>
-  </Card.Body>
-  <Card.Footer>
-    <AssignToStudentButton examId={examData.exam.id} />
-    <NavLink to={`scores/${examData.exam.id}`}>
-      <Button variant="outline" size="sm">
-        Scores
-      </Button>
-    </NavLink>
-  </Card.Footer>
-</Card.Root>;
