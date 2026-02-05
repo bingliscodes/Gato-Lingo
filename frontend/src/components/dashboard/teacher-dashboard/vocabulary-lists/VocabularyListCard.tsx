a5a1369dc79b91433d4d79c5ef7000ff5b3e35b1
Benjamin Inglis <bjammin134@gmail.com>
2026-02-05 13:12:02 -0800

    Display vocabulary items as table in list card

--- frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/VocabularyListCard.tsx
+++ frontend/src/components/dashboard/teacher-dashboard/vocabulary-lists/VocabularyListCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex, Text } from "@chakra-ui/react";
+import { Card, Flex, Text, Table } from "@chakra-ui/react";
 
 import { type VocabularyListResponse } from "@/utils/apiCalls";
 
@@ -17,6 +17,25 @@ export default function VocabularyListCard({
       <Card.Body>
         <Text fontWeight="bold">Description:</Text>
         <Text>{vocabularyListData.description}</Text>
+        <Text my={2} textAlign="center" fontWeight="bold">
+          Vocabulary
+        </Text>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Word</Table.ColumnHeader>
+              <Table.ColumnHeader>Translation</Table.ColumnHeader>
+              <Table.ColumnHeader>Part of Speech</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          {vocabularyListData.items.map((item) => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.word}</Table.Cell>
+              <Table.Cell>{item.translation}</Table.Cell>
+              <Table.Cell>{item.part_of_speech}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Root>
       </Card.Body>
       <Card.Footer></Card.Footer>
     </Card.Root>
