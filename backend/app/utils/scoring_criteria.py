001850c3690f1f04d500bc2eed1937382c631854
Benjamin Inglis <bjammin134@gmail.com>
2026-02-18 11:22:27 -0800

    Add oral proficiency scoring categories from Langauge Assessment 3rd edition

---
+++ backend/app/utils/scoring_criteria.py
@@ -0,0 +1,42 @@
+oral_proficiency_scoring_categories = {
+    "1": {
+        "Grammar": "Errors in grammar arefrequent, but speaker canbe understood by a native speaker used to dealing with foreigners attempting to speak his or her language.",
+        "Vocabulary": "Speaking vocabulary inadequate to express anything but the most elementary needs",
+        "Comprehension": "Within the scope of very limited language experience, can understand simple questions and statements if delivered with slowed speech, repetition,or paraphrase.",
+        "Fluency": "No specific fluency description. Refer to other four language areas for implied level of fluency",
+        "Pronunciation": "Errors in pronunciation are frequent but can be understood by a native speaker used to dealing with foreigners attempting to speak his or her language.",
+        "Task": "Can ask and answer questions on very familiar topics. Able to satisfy routine travel needs and minimum courtesy requirements. (Should be able to order a simple meal, ask for shelter or lodging, ask for and give simple directions, make purchases, and tell time.)"
+    },
+    "2": {
+        "Grammar": "Can usually handle elementary constructions quite accuratelyy but does not have throrough or confident control of the grammar",
+        "Vocabulary": "Has speaking vocabulary sufficient to express oneself simply with some circumlocutions",
+        "Comprehension": " Can get the gist of most conversations of nontechn ical subjects (i.e., topics that require no specialized knowledge).",
+        "Fluency": "Can handle with confidence but not with facility most social situations, including introductions and casual conversations about current events, as well as work, family, and autobiographical information.",
+        "Pronunciation": "Accent is intelligible though often quite faulty.",
+        "Task": "Able to satisfy routine social demands and work requirements; needs help in handling any complication or difficulties."
+    },
+    "3":{
+        "Grammar": " Control of grammar is good. Able to speak the language with sufficient structural accuracy to participate effectively in most formal and informal conversations on practical, social, and professional topics",
+        "Vocabulary": "Able to speak the language with sufficient vocabulary to participate effectively in most formal and informal conversations on practical, social, and professional topics. Vocabulary is broad enough that he or she rarely has to grope for a word.",
+        "Comprehension": "Comprehension is quite complete at a normal rate of speech",
+        "Fluency": "Can discuss particular interests of competence with reasonable ease. Rarely has to grope for words.",
+        "Pronunciation": "Errors never interfere with understanding and rarely disturb the native speaker. Accent may be obviously nonnative.",
+        "Task": "Can participate effectively in most formal and informal conversations on practical, social, and professional topics."
+    },
+    "4": {
+        "Grammar": "Able to use the language accurately on all levels normally pertinent to professional needs. Errors in grammar are quite rate",
+        "Vocabulary": "Can understand and participate in any conversation within the range of ones's experience with a high degree of precision of vocabulary",
+        "Comprehension": "Can understand any conversation within the range of one's experience",
+        "Fluency": "Able to use the language fluently on all levels normally pertinent to professional needs. Can participate in any conversation within the range of one's experience with a high degree of fluency.",
+        "Pronunciation": "Errors in pronunciation are quite rare",
+        "Task": "Would rarely be taken for a native speaker but can respond appropriately even in unfamiliar situations. Can handle informal interpreting from and into language."
+    },
+    "5": {
+        "Grammar": "Equivalent to that of an educated native speaker",
+        "Vocabulary": "Speech on all levels is fully accepted by educated native speakers in all its features including breadth of vocabulary and idoms, colloquialisms, and pertinent cultural references",
+        "Comprehension": "Equivalent to that of an educated native speaker",
+        "Fluency": "Has complete fluency in the language such that speech is fully accepted by educated native speakers.",
+        "Pronunciation": "Equivalent to and fully accepted by educated native speakers.",
+        "Task": "Speaking proficiency equivalent to that of an educated native speaker."
+    }   
+}
